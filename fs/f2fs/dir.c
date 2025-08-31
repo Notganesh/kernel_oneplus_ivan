b49ffaa07c3b38b8c1f82e5d1ba51e5b0fab8f86
Your Name <you@example.com>
2025-08-31 01:49:01 -0400

    Merge tag `v4.14.200` of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -1020,7 +1020,8 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
 			f2fs_warn(sbi, "%s: corrupted namelen=%d, run fsck to fix.",
 				  __func__, le16_to_cpu(de->name_len));
-			set_sbi_flag(sbi, SBI_NEED_FSCK);
+			//set_sbi_flag(sbi, SBI_NEED_FSCK);
+			set_sbi_flag(F2FS_I_SB(d->inode)->sb->s_fs_info, SBI_NEED_FSCK);
 			err = -EFSCORRUPTED;
 			goto out;
 		}
